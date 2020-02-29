a565bc53e3ea2810351ec933ab12276ed1f5404a
shri <snehithudu.com@gmail.com>
2020-03-01 00:17:00 +0530

    webgifts app created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainlayoutComponent } from './mainlayout/mainlayout.component';
+import { MenuComponent } from './menu/menu.component';
+import { ThemesComponent } from './themes/themes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainlayoutComponent,
+    MenuComponent,
+    ThemesComponent
   ],
   imports: [
     BrowserModule,
