8c98f883f82ec111b09086f236b0869dd5b727d5
NarasimhaDurga2380 <durgamnprasad@gmail.com>
2020-03-08 19:26:40 +0530

    dfsd

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,31 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
 
-const routes: Routes = [];
+import { ThemesComponent } from "./themes/themes.component";
+import { MarriageComponent } from "./marriage/marriage.component";
+
+const routes: Routes = [
+  {
+    path: "birthday",
+    component: ThemesComponent
+  },
+  {
+    path: "marriage",
+    component: MarriageComponent
+  },
+  {
+    path: "",
+    redirectTo: "/birthday",
+    pathMatch: "full"
+  },
+  {
+    path: "**",
+    redirectTo: "/birthday"
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { MainlayoutComponent } from './mainlayout/mainlayout.component';
 import { MenuComponent } from './menu/menu.component';
 import { ThemesComponent } from './themes/themes.component';
+import { MarriageComponent } from './marriage/marriage.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainlayoutComponent,
     MenuComponent,
-    ThemesComponent
+    ThemesComponent,
+    MarriageComponent
   ],
   imports: [
     BrowserModule,
